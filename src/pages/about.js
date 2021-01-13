760fe2655f099c5ccb11ff484af22af02956e69c
sakicmilan <sakic.milan@hotmail.com>
2021-01-13 23:28:33 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+function about() {
+  return (
+    <div>
+      About us <Link to="/contact">Contact</Link>
+    </div>
+  )
+}
+
+export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>Hello there I am Milan</h2>
+      <p>
+        Need a developer <Link to="/contact">contact me</Link>
+      </p>
+    </div>
+  )
 }
+
+export default IndexPage
